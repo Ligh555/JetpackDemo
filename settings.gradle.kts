3f93838c3193f0ceb5c49074c5211ff74304b288
Light555 <2277495183@qq.com>
2023-09-25 00:49:20 +0800

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,41 @@
+pluginManagement {
+    repositories {
+        google()
+        maven {
+            setUrl("https://maven.aliyun.com/repository/google")
+        }
+        maven {
+            setUrl("https://maven.aliyun.com/repository/public")
+        }
+        maven {
+            setUrl("https://maven.aliyun.com/repository/jcenter")
+        }
+        maven {
+            setUrl("https://maven.aliyun.com/repository/gradle-plugin")
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        maven {
+            setUrl("https://maven.aliyun.com/repository/google")
+        }
+        maven {
+            setUrl("https://maven.aliyun.com/repository/public")
+        }
+        maven {
+            setUrl("https://maven.aliyun.com/repository/jcenter")
+        }
+        maven {
+            setUrl("https://maven.aliyun.com/repository/gradle-plugin")
+        }
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "JetpackDemo"
+include(":app")
